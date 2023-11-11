9e0b82f076c0fd2ce524fb7041f09a53d4b737a4
DmGle <dimon.gleda1@gmail.com>
2023-11-11 22:51:44 +0200

    node_modules

--- parcel-project-template-main/src/js/01-gallery.js
+++ parcel-project-template-main/src/js/01-gallery.js
@@ -1,5 +1,49 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+let lightboxInstance = null;
+
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+gallery.addEventListener("click", handleClick);
+
+function createMarkup(arr) {
+    return arr.map(({preview, original, description}) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+            </a>
+        </li>
+    `).join("");
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    if (event.target.classList.contains('gallery__image')) {
+        const imageURL = event.target.getAttribute('data-source');
+        const imageAlt = event.target.getAttribute('alt');
+
+        lightboxInstance = basicLightbox.create(`
+            <img src="${imageURL}" alt="${imageAlt}">
+        `);
+        lightboxInstance.show();
+
+        // Додавання обробника подій для натискання клавіші "Escape"
+        document.addEventListener('keydown', handleKeyPress);
+    }
+}
+
+function handleKeyPress(event) {
+    if (event.key === 'Escape') {
+        if (lightboxInstance) {
+            lightboxInstance.close();
+            lightboxInstance = null;
+            // Видалення обробника подій після закриття лайтбокса
+            document.removeEventListener('keydown', handleKeyPress);
+        }
+    }
+}
 
 console.log(galleryItems);
