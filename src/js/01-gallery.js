a740f19599cc9eac1c063c6eac2d1b35f34f6dee
DmGle <dimon.gleda1@gmail.com>
2023-11-15 21:33:14 +0200

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+
+
+const createGalleryMarkup = items => {
+  return items
+    .map(({ preview, original, description }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+            </a>
+        </li>
+    `)
+    .join('');
+};
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+gallery.addEventListener('click', (event) => {
+  if (event.target.classList.contains('gallery__image')) {
+    event.preventDefault();
+
+    const imageURL = event.target.getAttribute('data-source');
+    const imageAlt = event.target.getAttribute('alt');
+
+    lightbox.open({
+      items: [{
+        src: imageURL,
+        title: imageAlt
+      }]
+    });
+  }
+});
 
 console.log(galleryItems);
