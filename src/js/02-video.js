2ee6f131bbd561e2e1e13ed20f51fd0b80b3ba57
DmGle <dimon.gleda1@gmail.com>
2023-11-15 21:57:54 +0200

    03-feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const localStorageKey = 'videoplayer-current-time';
+
+// Отримання елемента, на якому буде ініціалізований плеєр
+const playerElement = document.getElementById('vimeo-player');
+const player = new Player(playerElement);
+
+// Ініціалізація плеєра та відстеження події timeupdate
+player.ready().then(() => {
+  // Отримання часу відтворення з локального сховища та відновлення його
+  const storedTime = localStorage.getItem(localStorageKey);
+  if (storedTime) {
+    player.setCurrentTime(parseFloat(storedTime));
+  }
+
+  player.on('timeupdate', throttle(() => {
+    // Відстеження події timeupdate та збереження часу в локальному сховищі
+    const currentTime = player.currentTime;
+    localStorage.setItem(localStorageKey, currentTime);
+  }, 1000));
+});
+
+// Під час перезавантаження сторінки методом setCurrentTime() відновлюємо відтворення зі збереженої позиції
+window.addEventListener('beforeunload', () => {
+  player.getCurrentTime().then((currentTime) => {
+    localStorage.setItem(localStorageKey, currentTime);
+  });
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+// Відстеження події input та збереження значень полів у локальне сховище
+form.addEventListener('input', throttle(() => {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}, 500));
+
+// Заповнення полів форми зі збереженими значеннями під час завантаження сторінки
+document.addEventListener('DOMContentLoaded', () => {
+  const storedData = localStorage.getItem(localStorageKey);
+  if (storedData) {
+    const formData = JSON.parse(storedData);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+});
+
+// Очищення сховища та виведення даних форми під час сабміту
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+
+  console.log('Form Data:', formData);
+
+  // Очистка сховища та полів форми
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
 
